8a9cd536ad5919998192eb7f257c39b7017d8715
Hieu Phung <phungminhhieu1206@gmail.com>
2022-11-15 10:56:54 +0700

    create project, add rn vector icon

---
+++ App.tsx
@@ -0,0 +1,28 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+  useColorScheme,
+} from 'react-native';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import HomeScreen from './src/screens/HomeScreen';
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+      />
+      <HomeScreen/>
+    </SafeAreaView>
+  );
+};
+
+export default App;
