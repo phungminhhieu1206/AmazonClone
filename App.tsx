c730f8f794f9a764fd7edf33ee262a9a5bce5654
Hieu Phung <phungminhhieu1206@gmail.com>
2022-11-16 11:36:41 +0700

    update navigation

--- App.tsx
+++ App.tsx
@@ -1,29 +1,29 @@
 /* eslint-disable prettier/prettier */
+import 'react-native-gesture-handler';
 import React from 'react';
 import {
-  SafeAreaView,
   StatusBar,
   useColorScheme,
+  View,
 } from 'react-native';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
-import ProductScreen from './src/screens/ProductScreen';
-// import HomeScreen from './src/screens/HomeScreen';
+import Router from './src/navigation';
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    flex: 1
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
+    <View style={backgroundStyle}>
       <StatusBar
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
       />
-      {/* <HomeScreen/> */}
-      <ProductScreen/>
-    </SafeAreaView>
+      <Router/>
+    </View>
   );
 };
 
